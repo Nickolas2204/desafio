97a7a92e932b76c665ce037c2676423d18b0a21f
Nickolas2204 <nickolasandrew2204@gmail.com>
2024-12-25 18:27:37 -0300

    Desafio Alura

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,9 @@
+alert("Boas vindas ao nosso site!");
+let nome = prompt("Nome de Usuário");
+let idade = prompt("Qual sua idade ?");
+
+if (idade >= 18) {
+    alert("Pode tirar a habilitação!");
+} else {
+    alert("Não pode tirar a habilitação!");
+}
